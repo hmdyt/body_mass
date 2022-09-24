99760f1d0ec4359f03e9c85f38f531dac47c3238
hmdyt <konnyaku108@gmail.com>
2022-09-24 19:24:14 +0900

    linter&formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      aaa
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div className="App">aaa</div>;
+};
 
 export default App;
--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCMCpCtx_RAfGNxdZF_iK4TLKualKamsiE",
   authDomain: "body-mass-c8b99.firebaseapp.com",
@@ -13,9 +9,11 @@ const firebaseConfig = {
   storageBucket: "body-mass-c8b99.appspot.com",
   messagingSenderId: "954505292871",
   appId: "1:954505292871:web:26ddd8e07d8735cd689562",
-  measurementId: "G-5RHR0Z1JZX"
+  measurementId: "G-5RHR0Z1JZX",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const _analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
+
+export default analytics;
