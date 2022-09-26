4301217f997c42a72c747707ce78097b668a3292
hmdyt <konnyaku108@gmail.com>
2022-09-26 09:43:07 +0900

    body mass model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import BodyMassModel from "./model/BodyMass";
+import BodyMassModel, * as BodyMass from "./model/BodyMass";
 
 const App: React.FC = () => {
-  return <div className="App">aaa</div>;
+  const [bodyMasses, setBodyMasses] = useState<BodyMassModel[]>([]);
+
+  useEffect(() => {
+    void (async () => {
+      const bodyMassesResponse = await BodyMass.all();
+      setBodyMasses(bodyMassesResponse);
+    })();
+  }, []);
+
+  return (
+    <div className="App">
+      {bodyMasses.map((bodyMass) => (
+        <>
+          {bodyMass.created.toDate().toLocaleString()} {bodyMass.mass}
+          <br />
+        </>
+      ))}
+    </div>
+  );
 };
 
 export default App;
